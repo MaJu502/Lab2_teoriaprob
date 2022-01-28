f5caf2a67bd2fb387f96ee742986baf544679af6
Nes15442 <60157586+Nes15442@users.noreply.github.com>
2022-01-28 17:15:08 -0600

    Update Ejercicio_2.py
    Cambios en la implementación del ejercicio 2.
    Ahora se utiliza la librería Itertools

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -1,31 +1,40 @@
 
-# lista donde se guardaran los numeros primos
-primos:list = []
 
-for i in range(1, 101):
+# Se importa la libreria itertools
+import itertools
 
-    # Se incluyen los numeros de 1 al 100
-    primos.append(i)
+primos:list = [] # lista donde se guardaran los numeros primos
+conteo = itertools.count(1, 1) # Conteo de 1 en 1
+    
+# Se recorren los numeros del 1 al 100 en el conteo
+for i in conteo:
     
     # Se excluye el numero 1
     if i == 1:
-        primos.remove(i) 
+        continue
 
     # Se excluyen los numeros multiplos de 2 mayores que 2
     elif i % 2 == 0 and i != 2:
-        primos.remove(i) #quita el elemento de la lista primos
-
+        continue
+        
     # Se excluyen los numeros multiplos de 3 mayores que 3
     elif i % 3 == 0 and i != 3:
-        primos.remove(i)
+        continue
     
     # Se excluyen aquellos numeros multiplos de 5
     elif i % 5 == 0 and i != 5:
-        primos.remove(i)
+        continue 
 
     # Se excluyen aquellos numeros multiplos de 7
     elif i % 7 == 0 and i != 7:
-        primos.remove(i)   
+        continue
+
+    # Se evitan los numeros mayores que 100
+    if i >= 100:
+        break
+    
+    # Se incluyen unicamente los numeros primos de 1 al 100
+    primos.append(i)
 
 
-print(f'Lista de numeros primos < 100: \n{primos}\nlargo: {len(primos)}') # Imprime lista de numeros primos menores a 100
+print(f'Lista de numeros primos < 100: \n{primos}\nConteo de numeros primos menores a 100: {len(primos)}') # Imprime lista de numeros primos menores a 100
